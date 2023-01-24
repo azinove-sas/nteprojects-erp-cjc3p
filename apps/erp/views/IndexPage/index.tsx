45772b67e02d3b3cc8fe2fb066655a4058878f4e
Eiermann Patrick <eiermann.patrick.pro@outlook.com>
2023-01-24 14:59:32 +0300

    feat: adding template

---
+++ apps/erp/views/IndexPage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IndexPageType { }
+
+const IndexPage = ({ ...props }: IndexPageType) => {
+  return (
+    <>
+      Azinove Template
+    </>
+  );
+};
+
+export default IndexPage;
