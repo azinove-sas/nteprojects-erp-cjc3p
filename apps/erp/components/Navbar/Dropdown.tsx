3913f5928d08d81762aa683402ec89d120977cc8
Eiermann Patrick <eiermann.patrick.pro@outlook.com>
2023-01-28 04:26:58 +0300

    feat: adding navbar

--- apps/erp/components/Navbar/Dropdown.tsx
+++ apps/erp/components/Navbar/Dropdown.tsx
@@ -1,14 +1,14 @@
 import MenuItems from "./MenuItems";
 import { Box } from "rebass";
 
-const Dropdown = ({ submenus, dropdown, depthLevel }) => {
+const Dropdown = ({ submenus, dropdown, depthLevel }: any) => {
   depthLevel = depthLevel + 1;
 
   const dropdownClass = depthLevel > 1 ? "dropdown-submenu" : "";
 
   return (
     <ul className={`dropdown ${dropdownClass} ${dropdown ? "show" : ""}`}>
-      {submenus.map((submenu, index) => (
+      {submenus.map((submenu: any, index: number) => (
         <MenuItems items={submenu} key={index} depthLevel={depthLevel} />
       ))}
     </ul>
